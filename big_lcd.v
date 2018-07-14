1897bdd72e27958148f2e22bc9c6119a8348ea44
Mikhail <skatanik@users.noreply.github.com>
2018-07-15 00:53:55 +0300

    Update big_lcd.v

--- big_lcd.v
+++ big_lcd.v
@@ -46,14 +46,14 @@ if(!reset_n)
         end
 end
 
-assign lcd_read = (counter_hs >= 11'd44 && counter_hs < 11'd845 && counter_vs >= 11'd23  && counter_vs < 11'd503) ;
+assign lcd_read = (counter_hs >= 11'd45 && counter_hs < 11'd845 && counter_vs >= 11'd23  && counter_vs < 11'd503) ;
 
 assign VSYNC = (counter_vs >= 11'd0 && counter_vs < 11'd10) ? 1'b0 : 1'b1;
-assign HSYNC = (counter_hs >= 11'd0 && counter_hs < 11'd10) ? 1'b0 : 1'b1;
+assign HSYNC = (counter_hs >= 11'd0 && counter_hs < 11'd20) ? 1'b0 : 1'b1;
 
-assign R = !no_data_available ? lcd_readdata[23:16] : 8'h34;
-assign G = !no_data_available ? lcd_readdata[15:8] : 8'h34;
-assign B = !no_data_available ? lcd_readdata[7:0] : 8'h34;
+assign R = !no_data_available ? lcd_readdata[23:16] : 8'hFF;
+assign G = !no_data_available ? lcd_readdata[15:8] : 8'hFF;
+assign B = !no_data_available ? lcd_readdata[7:0] : 8'hFF;
 
 assign data_request = (counter_hs == 810 && counter_vs == 11'd504);
 
